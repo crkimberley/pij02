e9d79e3a79c7f71341a8f0f38a1bc8d010d71d08
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-07-22 17:57:57 +0100

    Initial commit

---
+++ src/Q13.groovy
@@ -0,0 +1,20 @@
+import static java.lang.Math.sqrt
+
+/**
+ * Created by crk on 20/07/2016.
+ */
+int number = 2
+while (number <=1000) {
+    boolean prime = true
+    int i = 2
+    while (i <= (int) sqrt(number) && prime) {
+        if (number % i == 0) {
+            prime = false
+        }
+        i++
+    }
+    if (prime) {
+        println number
+    }
+    number++
+}
