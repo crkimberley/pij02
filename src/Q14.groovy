884683eb060cf5d2dd2f9af9af74a6a9c037420a
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-07-22 18:04:10 +0100

    Q13 - added parentheses to 2nd while loop

--- src/Q14.groovy
+++ src/Q14.groovy
@@ -5,7 +5,7 @@ int number = 2
 while (count < 1000) {
     boolean prime = true
     int i = 2
-    while (i <= (int) sqrt(number) && prime) {
+    while (i <= ((int) sqrt(number)) && prime) {
         if (number % i == 0) {
             prime = false
         }
