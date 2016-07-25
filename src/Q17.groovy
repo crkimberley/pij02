0aa0a95fa71315055da2aa6f7c9d4529a88efc97
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-07-25 22:19:17 +0100

    Q17 - comments added

--- src/Q17.groovy
+++ src/Q17.groovy
@@ -8,8 +8,9 @@ int lowPrime, highPrime
 print "Enter number to find closest primes: "
 int number = input.nextInt()
 print "Closest prime(s) to " + number + " = "
-
+// Special cases for numbers <=2
 if (number > 2) {
+    // routine runs twice - up & down, using upOrDown variable -1 or +1
     int upOrDown = -1
     while (upOrDown < 2) {
         int possiblePrime = number + upOrDown
@@ -34,12 +35,13 @@ if (number > 2) {
         }
         upOrDown = upOrDown + 2
     }
-
+    // print low & high primes if equidistant, else closest
     if (number - lowPrime == highPrime - number) {
         println lowPrime + " & " + highPrime
     } else {
         println (number - lowPrime < highPrime - number ? lowPrime : highPrime)
     }
+    // special cases for numbers <= 2
 } else {
     if (number == 2) {
         println "3"
