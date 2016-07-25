d8922525ea12e5bbf16cc8dc87044d18b808a9ed
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-07-25 22:35:06 +0100

    Q16 - refactored, tidying up, simplifying

--- src/Q16.groovy
+++ src/Q16.groovy
@@ -3,6 +3,7 @@ import static java.lang.Math.abs
 /**
  * Created by crk on 25/07/2016.
  */
+// No error checking on inputs for simplicity
 Scanner input = new Scanner(System.in)
 println "Rock, Paper, Scissors - enter r for rock, p for paper, s for scissors"
 println "Press return after both players have typed their selection"
@@ -11,24 +12,12 @@ String turn
 while (abs(score1 - score2) <= 3) {
     println "Player 1 selection - Player 2 selection - Return..."
     turn = input.next()
-    if (turn.charAt(0) == 'r') {
-        if (turn.charAt(1) == 's') {
-            score1++
-        } else if (turn.charAt(1) == 'p') {
-            score2++
-        }
-    } else if (turn.charAt(0) == 'p') {
-        if (turn.charAt(1) == 'r') {
-            score1++
-        } else if (turn.charAt(1) == 's') {
-            score2++
-        }
-    } else {
-        if (turn.charAt(1) == 'p') {
-            score1++
-        } else if (turn.charAt(1) == 'r') {
-            score2++
-        }
+    if ((turn.charAt(0) == 'r' && turn.charAt(1) == 's')
+    || (turn.charAt(0) == 'p' && turn.charAt(1) == 'r')
+    || (turn.charAt(0) == 's' && turn.charAt(1) == 'p')) {
+        score1++
+    } else if (turn.charAt(0) != turn.charAt(1)) {
+        score2++
     }
     println "Player 1: " + score1 + "\nPlayer 2: " + score2
 }
