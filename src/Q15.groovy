a9485db1f8eef41ec16ff53e3f06fab150cf705a
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-07-25 15:37:38 +0100

    Q15 - changed Math.random to Random().nextInt() for random number generation

--- src/Q15.groovy
+++ src/Q15.groovy
@@ -1,26 +1,24 @@
-import static java.lang.Math.abs
-import static java.lang.Math.random
-
 /**
  * Created by crk on 24/07/2016.
  */
 Scanner input = new Scanner(System.in)
-int randomNumber = abs(random() * 1001)
-println randomNumber
+//int randomNumber = abs(random() * 1001)
+int randomNumber = new Random().nextInt(1001)
 int guess
-println "Guess a random number between 0 & 1,000"
+print "This program picks a random number between 0 & 1,000 - "
 int count = 1
 while (true) {
-    print " - guess the number: "
+    print "guess the number: "
     guess = input.nextInt()
     if (guess == randomNumber) {
         println "Correct! You took " + count + " guesses"
         break
     }
+    print "No - " + guess + " is too "
     if (guess < randomNumber) {
-        print " too low\t"
+        print "low, "
     } else {
-        print " too high\t"
+        print "high, "
     }
     count++
 }
